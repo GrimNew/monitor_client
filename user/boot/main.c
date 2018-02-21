02c479c7eec2f758a1639f9859deac099acea5dd
GrimNew <326408489@qq.com>
2018-02-21 18:12:59 +0800

    2018.12.21

--- user/boot/main.c
+++ user/boot/main.c
@@ -1,4 +1,6 @@
 #include "stm32f10x.h"
+#include <usart.h>
+#include <delay.h>
 
 int main(void)
 {
--- user/inc/usart.h
+++ user/inc/usart.h
@@ -1,2 +1,5 @@
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_gpio.h>
+
+void usart_Init();
+void usart_SendString(USART_TypeDef* USARTx,u32 *Tx_string);
